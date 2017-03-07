834b7f6cea6785329d87948020e13fedb9842e78
Ryan McAlpin and Riley Watts <ryan@>
2017-03-06 16:46:19 -0800

    tweak random numbers

--- js/script.js
+++ js/script.js
@@ -24,6 +24,36 @@ Character.prototype.healthLoss = function() {
   this.health -= 1;
 }
 
+var prompt = function(inputNumber) {
+  switch (inputNumber) {
+    case 1:
+      travel();
+      break;
+    case 2:
+      rest();
+      break;
+    case 3:
+      hunt();
+      break;
+  }
+}
+function fates(roll) {
+  var charIndex = rollNumber(0,5);
+  if(roll<=7) {
+
+    caravan.party[charIndex].diseased=true;
+  }else if(roll<=15){
+    caravan.party[charIndex].health=0;
+  }
+
+}
+
+function rollNumber(min, max) {
+  min = Math.ceil(min);  //inclusive
+  max = Math.floor(max); //exclusive
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var rest = function() {
   caravan.food -= 2 * caravan.party.length;
   caravan.party.forEach(function (element) {
@@ -39,6 +69,9 @@ var hunt = function() {
 }
 
 var travel = function() {
+  var roll = rollNumber(1,101);
+  console.log(roll);
+  fates(roll);
   caravan.food -= 2 * caravan.party.length;
   caravan.party.forEach(function (element) {
     element.healthLoss();
@@ -47,8 +80,9 @@ var travel = function() {
   game.daysLeft--;
 }
 
-
 var char1 = new Character("Ryan");
 var char2 = new Character("Riley");
 var char3 = new Character("Chris");
-caravan.party.push(char1, char2, char3);
+var char4 = new Character("Gloria");
+var char5 = new Character("Megan");
+caravan.party.push(char1, char2, char3, char4, char5);
